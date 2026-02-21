ec8b1d4e427fc82fb6a6538a6561b27583f0b3a6
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-02-21 06:51:25 +0600

    gh=pages branch added

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      "Cache-Control": "no-store",
-    },
-  },
+  base: "/ramadan_calender/",
 });
